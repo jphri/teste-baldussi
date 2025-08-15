d3b8f5a8d41cb503ae662f2737f4f4fd642f484f
João Pedro Hrihorowitsch Silva <jp.hri.silva@gmail.com>
2025-08-15 11:13:44 -0300

    adicionado calls

--- backend/baldussi_backend/routers/calls.py
+++ backend/baldussi_backend/routers/calls.py
@@ -10,3 +10,8 @@ async def raw_get_fn(count: int = 100, empresa_id: str | None = None):
         "count": len(response),
         "data": response
     }
+
+@router.get("/get")
+async def get_fn(page: int = 1, limit: int = 100, empresa_id: str | None = None, date_from: str | None = None, date_to: str | None = None, destino: str | None = None, sip_code: str | None = None, q: str | None = None):
+    response = call_api.calls(page, limit, empresa_id, date_from, date_to, destino, sip_code)
+    return response
--- backend/baldussi_backend/routers/calls_test.py
+++ backend/baldussi_backend/routers/calls_test.py
@@ -9,3 +9,13 @@ def test_calls_raw():
     response = client.get('/calls/raw')
 
     assert not response.is_error
+
+def test_calls():
+    """
+    Test calls
+    """
+
+    client = TestClient(app)
+    response = client.get('/calls/get')
+
+    assert not response.is_error
