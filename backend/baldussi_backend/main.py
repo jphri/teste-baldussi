7c40c61e3287103893010682bd88571b3dcb0fb8
João Pedro Hrihorowitsch Silva <jp.hri.silva@gmail.com>
2025-08-15 17:52:55 -0300

    Adicionado cors

--- backend/baldussi_backend/main.py
+++ backend/baldussi_backend/main.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 import baldussi_backend.routers.calls as calls
 
 app = FastAPI(title="Baldussi API teste")
 
 app.include_router(calls.router)
+
+# Configuração do CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # ou ["http://localhost:3000"] se quiser restringir
+    allow_credentials=True,
+    allow_methods=["*"],  # GET, POST, etc.
+    allow_headers=["*"],  # Cabeçalhos permitidos
+)
 
 @app.get("/ping")
 def ping_call():
