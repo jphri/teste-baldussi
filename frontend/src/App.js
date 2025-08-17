629c9f579f65d64e1413b6a2c0c49cab63d80d82
João Pedro Hrihorowitsch Silva <jp.hri.silva@gmail.com>
2025-08-17 11:29:14 -0300

    Testando router no front

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import {Routes, Route} from 'react-router-dom';
+
+function Greet(props) {
+  return (<h1>Hello, {props.name}</h1>)
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route path="/" element={<Greet name="Root"/>}/>
+        <Route path="/something" element={<Greet name="Something else"/>}/>
+        <Route path="/asdf" element={<Greet name="What?"/>}/>
+      </Routes>
     </div>
   );
 }
