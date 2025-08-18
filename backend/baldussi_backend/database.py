cb71b23b90cd415490bc74d90b2be5174f82ed98
João Pedro Hrihorowitsch Silva <jp.hri.silva@gmail.com>
2025-08-17 21:32:32 -0300

    Adicionado postgres no backend e no docker

--- backend/baldussi_backend/database.py
+++ backend/baldussi_backend/database.py
@@ -1,9 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
-DATABASE_URL = "sqlite:///./teste.db"
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+DATABASE_URL = os.getenv('DATABASE_URL', "sqlite:///./teste.db")
+engine = create_engine(DATABASE_URL)
 Base = declarative_base()
 SessionLocal = sessionmaker(bind=engine)
 
